cce46254f5d21c14b1f0fa2db84ad7d768ff5cec
Vika <72411145+vika090@users.noreply.github.com>
2026-03-07 11:43:25 +0000

    Задача 1

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -1,3 +1,14 @@
-#include <iostream>
+/*# Задача 1
 
-int main() { return 0; }
+Дано целое число и отсортированый массив целых чисел, нужно найти 2 числа из массива которые в сумме дадут заданное число
+*/
+#include <iostream>
+#include <vector>
+#include "util.hpp"
+using namespace std;
+
+
+
+int main(){
+    return 0;
+}
--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -1,5 +1,46 @@
 #include <gtest/gtest.h>
 
-TEST(Test, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+#include <util.hpp>
+
+
+TEST(Test1, Simple) {
+    std::vector<int> v = {1, 2, 3, 4, 5, 6};
+    
+    auto result = fun(7, v);
+    EXPECT_EQ(result.first, 1);
+    EXPECT_EQ(result.second, 6);
 }
+
+TEST(Test2, Empty) {
+    std::vector<int> v = {};
+    
+    auto result = fun(7, v);
+    EXPECT_EQ(result.first, 0);
+    EXPECT_EQ(result.second, 0);
+}
+
+TEST(Test3, Small) {
+    std::vector<int> v = {2};
+    
+    auto result = fun(7, v);
+    EXPECT_EQ(result.first, 0);
+    EXPECT_EQ(result.second, 0);
+}
+
+TEST(Test4, No_pair) {
+    std::vector<int> v = {2, 5, 7, 8, 9, 14};
+    
+    auto result = fun(52, v);
+    EXPECT_EQ(result.first, -1);
+    EXPECT_EQ(result.second, -1);
+}
+
+TEST(Test5, Negative_number) {
+    std::vector<int> v ={-1, 3, -4, 8};
+
+    auto result = fun(-5, v);
+    EXPECT_EQ(result.first, -1);
+    EXPECT_EQ(result.second, -4);
+
+}
+

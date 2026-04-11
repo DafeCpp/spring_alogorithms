84175e7a5458bcd0895b38a15ec7a6afad190524
melanikot-mipt <kotova.mv@phystech.edu>
2026-04-11 08:36:09 +0000

    task_02

--- task_02/src/main.cpp
+++ task_02/src/main.cpp
@@ -21,33 +21,5 @@ int main() {
   while (is >> x) v.push_back(x);
 
   cout << get_border_index(v) << endl;
-  /*
-  if (v[1] == 1) cout << 0 << endl;
-  else if (v[N-2] == 0) cout << N-2 << endl;
-  else
-  {
-      int i{(N-1)/2};
-      while (v[i] != 0 && v[i+1] != 1)
-      {
-          if (v[i] == 0)
-          {
-              if (i+1 < v.size() && v[i+1] == 1)
-              {
-                  cout << i << endl;
-                  break;
-              }
-              i = (N-1 - i)/2;
-          } else
-          {
-              if (i-1 >= 0  && v[i-1] == 0)
-              {
-                  cout << i-1 << endl;
-                  break;
-              }
-              i /= 2;
-          }
-      }
-  };
-  */
   return 0;
 }

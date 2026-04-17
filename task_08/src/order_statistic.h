29e56924ea629fa58f6aae15fad18a3317a5ba59
Игорь Кодик <kodik.ia@phystech.edu>
2026-04-17 16:10:19 +0300

    done task_08

---
+++ task_08/src/order_statistic.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <vector>
+
+int GetOrderStatistic(std::vector<int> values, int order);
--- task_08/src/test.cpp
+++ task_08/src/test.cpp
@@ -1,6 +1,23 @@
-
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+#include "order_statistic.h"
+
+TEST(OrderStatisticTest, HandlesMedian) {
+  EXPECT_EQ(GetOrderStatistic({7, 10, 4, 3, 20}, 3), 7);
+}
+
+TEST(OrderStatisticTest, HandlesMinimum) {
+  EXPECT_EQ(GetOrderStatistic({7, 10, 4, 3, 20}, 1), 3);
+}
+
+TEST(OrderStatisticTest, HandlesMaximum) {
+  EXPECT_EQ(GetOrderStatistic({7, 10, 4, 3, 20}, 5), 20);
+}
+
+TEST(OrderStatisticTest, HandlesDuplicates) {
+  EXPECT_EQ(GetOrderStatistic({3, 1, 4, 1, 5, 9}, 4), 4);
+}
+
+TEST(OrderStatisticTest, HandlesNegativeNumbers) {
+  EXPECT_EQ(GetOrderStatistic({-5, 3, -10, 0, 7, -3}, 2), -5);
 }

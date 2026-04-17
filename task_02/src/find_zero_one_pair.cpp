ed6b859be23887da0d6d9acc4a4d8c00c0269847
Игорь Кодик <kodik.ia@phystech.edu>
2026-04-17 11:32:20 +0300

    done task_02

---
+++ task_02/src/find_zero_one_pair.cpp
@@ -0,0 +1,23 @@
+#include "./find_zero_one_pair.h"
+
+#include <stdexcept>
+
+int FindZeroOnePair(const std::vector<int>& values) {
+  if (values.size() < 2) {
+    throw std::invalid_argument("Array must contain at least two elements");
+  }
+  if (values[0] != 0) {
+    throw std::invalid_argument("The first element must be zero");
+  }
+  if (values[values.size()-1] != 1) {
+    throw std::invalid_argument("The last element must be zero");
+  }
+
+  for (int i = 0; i < static_cast<int>(values.size()) - 1; i++) {
+    if (values[i] == 0 && values[i + 1] == 1) {
+      return i;
+    }
+  }
+
+  return -1;
+}
---
+++ task_02/src/find_zero_one_pair.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <vector>
+
+int FindZeroOnePair(const std::vector<int>& values);
